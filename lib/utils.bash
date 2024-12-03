8e3b08b2058badd52f8981f7abebc1fd0033d265
Christopher Virtucio <christopher.virtucio@zendesk.com>
2024-12-03 08:32:12 -0500

    fixed sort

--- lib/utils.bash
+++ lib/utils.bash
@@ -16,7 +16,7 @@ curl_opts=(-fsSL)
 
 sort_versions() {
   sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+    LC_ALL=C sort -t "." -k1,1n -k2,2n -k3,3V -k1.1,1.1 | awk '{print $2}'
 }
 
 list_github_tags() {
